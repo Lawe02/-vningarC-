64be972b197e5d8f2605a86cee891da27cdc1e11
Lawe02 <lawza02@gmail.com>
2022-09-29 01:11:48 +0200

    Övning 7, Files

--- Class1.cs
+++ Class1.cs
@@ -9,115 +9,36 @@ namespace Övningar
     internal class Class1
     {
         public void Go()
-        { 
-
+        {
             while(true)
             {
-                Console.WriteLine("Skriv 1 för att logga in");
-                Console.WriteLine("Skriv 2 för att regga konto");
+                Console.WriteLine("Ange mappnamn");
+                string folderName = Console.ReadLine();
+                Console.WriteLine("Ange filtyp ex.. '.txt' ");
+                string extension = Console.ReadLine();
+                var files = FindFiles(folderName, extension);
 
-                string s = Console.ReadLine();
-                
-                switch (s)
+                foreach(string file in files)
                 {
-                    case "1":
-
-                        Console.WriteLine("Matan in användarnamn");
-                        string anv = Console.ReadLine();
-                        Console.WriteLine("Mata in lösenord");
-                        string lös = Console.ReadLine();
-                        bool login = Login(anv, lös);
-                        if (login)
-                            Console.WriteLine("Du är inloggad");
-                        else
-                            Console.WriteLine("Du är inte inloggad");
-                        break;
-
-                    case "2":
-
-                        User newuser = AddUser();
-
-                        if (newuser == null) 
-                        {
-                            Console.WriteLine("Användarnamnet finns redan");
-                            break;
-                        }
-                        else
-                        {
-                            using var sw = File.AppendText("accounts.txt");
-                            sw.WriteLine(newuser.username);
-                            sw.WriteLine(newuser.password);
-                            break;
-                        }
+                    var size = new FileInfo(file).Length;
+                    Console.WriteLine($"Namn: {file}, Sotrlek: {size} Bytes");
                 }
+
             }
         }
-        public bool Login(string nmn, string pss)
+
+        public List<string> FindFiles(string path, string exe)
         {
-            bool inloggad = false;
-            bool isUsername = true;
-            int i = 0;
-            int j = 0;
-            var lines = File.ReadLines("accounts.txt");
-            foreach(var line in lines)
+            var allFiles = new List<string>();
+            IEnumerable<string> allFilesInDirectory = Directory.EnumerateFiles(path);
+
+            foreach(var file in allFilesInDirectory)
             {
-                i++;
-                if (isUsername)
-                {
-                    if (line == nmn)
-                        j = i;
-
-                    isUsername = false;
-                }
-                else if (line == pss && i - j == 1)
-                {
-                    inloggad = true;
-                    break;
-                }
-                
-                else
-                    isUsername = true;
+                if (file.EndsWith(exe))
+                    allFiles.Add(file);
             }
-            return inloggad;
-        }
-
-        public User AddUser()
-        { 
-            var användare = new User();
-            //bool finnsRedan = false;
-
-            Console.WriteLine("Mata in nytt användarnamn");
-            string username = Console.ReadLine();
-
-            var lines = File.ReadLines("accounts.txt");            
-            if(lines.Count() == 0)
-            {
-                Console.WriteLine("Mata in nytt lösenord");
-                string password = Console.ReadLine();
-                användare.username = username;
-                användare.password = password;
-            }
-            else
-            {
-                foreach(var line in lines)
-                {
-                    if(username == line)
-                        return null;
-                    
-                }
-                Console.WriteLine("Mata in nytt lösenord");
-                string password = Console.ReadLine();
-                användare.username = username;
-                användare.password = password;
-                 
-            }
-            return användare;
+            return allFiles;
 
         }
-    }
-    public class User
-    {
-        public string username { get; set; }    
-        public string password { get; set; }    
     }
 }
