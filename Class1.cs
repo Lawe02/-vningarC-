7fd68ef992cc67a2c3ba89630bebf9dd9036d97f
Lawe02 <lawza02@gmail.com>
2022-09-29 14:54:19 +0200

    övning (7), Files

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,35 +11,86 @@ namespace Övningar
     {
         public void Go()
         {
+
+            List<Car> list = GetFromFile();
+      
             while(true)
             {
-                Console.WriteLine("Ange mappnamn");
-                string folderName = Console.ReadLine();
-                Console.WriteLine("Ange filtyp ex.. '.txt' ");
-                string extension = Console.ReadLine();
-                var files = FindFiles(folderName, extension);
+                Console.WriteLine("1. Skapa ny bil");
+                Console.WriteLine("2. ta bort bil");
+                Console.WriteLine("3. Avsluta programmet");
+                string val = Console.ReadLine();
 
-                foreach(string file in files)
+                switch (val)
                 {
-                    var size = new FileInfo(file).Length;
-                    Console.WriteLine($"Namn: {file}, Sotrlek: {size} Bytes");
+                    case "1":
+                        {
+                            Car car = AddCar();
+                            list.Add(car);
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            int i = 0;
+                            foreach(Car car in list)
+                            {
+                                Console.WriteLine($"{car} Id: {i}");
+                                i++;
+                            }
+                            Console.WriteLine("Ange id för vilken bil du vill ta bort");
+                            int id = int.Parse(Console.ReadLine());
+                            list.RemoveAt(id);
+    
+                            break;
+                        }
+
+                    case "3":
+                        {
+                            SaveAllCars(list);
+                            break;
+                        }
                 }
-
             }
         }
-
-        public List<string> FindFiles(string path, string exe)
+        public List<Car> GetFromFile()
         {
-            var allFiles = new List<string>();
-            IEnumerable<string> allFilesInDirectory = Directory.EnumerateFiles(path);
-
-            foreach(var file in allFilesInDirectory)
-            {
-                if (file.EndsWith(exe))
-                    allFiles.Add(file);
-            }
-            return allFiles;
-
+            string fileContent = File.ReadAllText("carsinjson.txt");
+            List<Car> list = JsonConvert.DeserializeObject<List<Car>>(fileContent);
+            return list;
         }
+        public void SaveAllCars(List<Car> cars)
+        {
+            string data = JsonConvert.SerializeObject(cars);
+            File.WriteAllText("carsinjson.txt", data);
+        }
+        public Car AddCar()
+        {             
+            Console.WriteLine("Ange märke");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ange model");
+            string model = Console.ReadLine();
+            Console.WriteLine("Ange årsmodell");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ange pris");
+            int price = int.Parse(Console.ReadLine()); 
+
+            var car = new Car() { Name = name, Model = model, Year = year, Price = price};
+
+            return car;
+        }
+    }
+    public class Car
+    {
+        public string Name { get; set; }    
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "  Model:  " + Model + "   Year: " + Year + "   Price " + Price ;
+        }
+
     }
 }
