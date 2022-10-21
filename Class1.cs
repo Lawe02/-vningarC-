1af91351dbf4413508159090ca72659ec1b7bafa
Lawe02 <lawza02@gmail.com>
2022-10-22 00:54:51 +0200

    Övning 1, Arv

--- Class1.cs
+++ Class1.cs
@@ -6,109 +6,37 @@ namespace Övningar
     {
         public void Go()
         {
-            List<Dog> list = GetFromFile();
 
-            while(true)
-            {
-                Console.WriteLine("1. Registrera hund, 2. Läs ut hundar baserat på svansLängd, 3. Ta bort hund, 4. Avlsuta programmet");
-                switch(Console.ReadLine())
-                    {
-
-                    case "1":
-                        Dog dog = CreateDog();
-                        list.Add(dog);
-                        break;
-                    
-                    case "2":
-                        Console.WriteLine("Enter minimum tale length");
-                        int length = Convert.ToInt32(Console.ReadLine());
-                        foreach(Dog d in list)
-                        {
-                            if(d.TaleLength >= (double)length)
-                            {
-                                Console.WriteLine(d);
-                            }
-                        }
-                        break;
-
-                    case "3":
-                        Console.WriteLine("Enter name of dog that will be deleted");
-                        string name = Console.ReadLine();
-                        bool deleted = false;
-                        foreach(Dog d in list.ToList())
-                        {
-                            if (d.Name == name)
-                            {
-                                list.Remove(d);
-                                deleted = true;
-                            }
-                        }
-                        if (!deleted)
-                            Console.WriteLine($"A dog with the name {name} could not be found in the list");
-
-                        break;
-
-                    case "4":
-                        SaveAllDogs(list);
-                        break;
-
-                }
-            }
         }
-        public void SaveAllDogs(List<Dog> list)
-        {
-            string content = JsonConvert.SerializeObject(list);
-            File.WriteAllText("TextFile1.json", content);
-        }
-        public List<Dog> GetFromFile()
-        {
-            var list = new List<Dog>();
-            string content = File.ReadAllText("TextFile1.json");
-            list = JsonConvert.DeserializeObject<List<Dog>>(content);
-            return list;
-        }
-        public Dog CreateDog()
-        {
-            Console.WriteLine("Enter DogName");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter DogRace");
-            string race = Console.ReadLine();
-            Console.WriteLine("Enter DogAge");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter DogWeight");
-            int weight = Convert.ToInt32(Console.ReadLine());
-            Dog dog = new Dog(name, race, age, weight);
-            return dog;
-        }
+
     }
-    public class Dog
+    public class Point
     {
-        private string _name;
-        private string _race;
-        private int _age;
-        private int _weight;
-        private double _taleLength;
+        private int _x;
+        private int _y;
+        public int X { get { return _x; } set { _x = value; } }   
+        public int Y { get { return _y; } set { _y = value; } }
 
-        public Dog(string name, string race, int age, int weight)
+        public Point(int y, int x)
         {
-            _name = name;
-            _race = race;
-            _age = age;
-            _weight = weight;
-            if (race != "tax")
-                _taleLength = (age * weight) / 10;
-            else _taleLength = 3.7;
+            _y = y;
+            _x = x;
         }
-        public string Name { get { return _name; } set { _name = value; } }
-        public double TaleLength { get { return _taleLength; } set { _taleLength = value; } } 
-        public string Race { get  { return _race; } set { _race = value; } }
-        public int Weight { get { return _weight; } set { _weight = value; } }
-        public int Age { get { return _age; } set { _age = value; } }
-        public override string ToString()
-        {
-            return $"Namn: {_name}, Ras: {_race}, Ålder: {_age}, Vikt: {_weight}, SvansLängd: {_taleLength}";
-        }
-
     }
-   
+    public class Shape
+    {
+        private Point _point;
+        public Point Point { get { return _point; } }
+
+        public virtual void Draw (){ }
+        public Shape(Point p)
+        {
+            _point = p;
+        }
+        public void GetPosition()
+        {
+            Console.WriteLine($"x = {_point.X}, y = {_point.X}");
+        }
+    }
 }
+
